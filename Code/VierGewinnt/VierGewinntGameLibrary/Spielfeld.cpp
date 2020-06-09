5ee597b08bfa299ca8d834980f4939522944011c
LucRome <64947931+LucRome@users.noreply.github.com>
2020-06-09 21:39:02 +0200

    Implementierung: spielfeld->isVoll();
     Hinzugefügt .TMP zu gitignore

--- Code/VierGewinnt/VierGewinntGameLibrary/Spielfeld.cpp
+++ Code/VierGewinnt/VierGewinntGameLibrary/Spielfeld.cpp
@@ -9,7 +9,26 @@ Spielfeld::~Spielfeld()
 {
 }
 
+bool Spielfeld::isVoll() const
+{
+    bool stillFree = false;
+    int spalte = 0, zeile = 0;
 
+    while (zeile < m_size && !stillFree) {  //sucht das Spielfeld nach und nach nach leeren Feldern ab, beendet wenn eins gefunden, oder wenn am Ende
+        if (!getPosition(spalte, zeile)) {
+            stillFree = true;
+        }
+        else {
+            spalte = (spalte + 1) % m_size;
+            if (spalte == 0) {
+                zeile++;
+            }
+        }
+    }
+
+
+    return !stillFree;
+}
 
 int Spielfeld::getSize() const
 {
