4a246c337885c027e0057d807baadb284ca2e969
LucRome <64947931+LucRome@users.noreply.github.com>
2020-06-14 14:35:00 +0200

    irgendwas in Regelwerk.h

--- Code/VierGewinnt/VierGewinntGameLibrary/Regelwerk.h
+++ Code/VierGewinnt/VierGewinntGameLibrary/Regelwerk.h
@@ -9,7 +9,8 @@ class Regelwerk
 public:
 	Regelwerk();
 	~Regelwerk();
-	std::shared_ptr<Spielsteine> waagerechtCheck(Spielfeld & spielfeld, std::shared_ptr<Spielsteine> meinStein); //meinStein -> Beispielstein für den Verwendeten?
+	/*vielleicht alle als static?*/
+	std::shared_ptr<Spielsteine> waagerechtCheck(Spielfeld& spielfeld, std::shared_ptr<Spielsteine> meinStein); //meinStein -> Beispielstein für den Verwendeten?
 	std::shared_ptr<Spielsteine> senkrechtCheck(Spielfeld& spielfeld, std::shared_ptr<Spielsteine> meinStein);  //Eher über Übergabe des Teams?
 	std::shared_ptr<Spielsteine> diagonalCheck(Spielfeld& spielfeld, std::shared_ptr<Spielsteine> meinStein);
 };
