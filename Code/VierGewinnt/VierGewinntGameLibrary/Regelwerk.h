06cd20a65ae7a3b8748a595649a9daf49df0ee48
LucRome <64947931+LucRome@users.noreply.github.com>
2020-06-22 14:45:28 +0200

    Regelwerkmethoden -> static

--- Code/VierGewinnt/VierGewinntGameLibrary/Regelwerk.h
+++ Code/VierGewinnt/VierGewinntGameLibrary/Regelwerk.h
@@ -10,7 +10,7 @@ public:
 	Regelwerk();
 	~Regelwerk();
 	/*vielleicht alle als static?*/
-	std::shared_ptr<Spielsteine> waagerechtCheck(Spielfeld& spielfeld, std::shared_ptr<Spielsteine> meinStein); //meinStein -> Beispielstein für den Verwendeten?
-	std::shared_ptr<Spielsteine> senkrechtCheck(Spielfeld& spielfeld, std::shared_ptr<Spielsteine> meinStein);  //Eher über Übergabe des Teams?
-	std::shared_ptr<Spielsteine> diagonalCheck(Spielfeld& spielfeld, std::shared_ptr<Spielsteine> meinStein);
+	static std::shared_ptr<Spielsteine> waagerechtCheck(Spielfeld& spielfeld, std::shared_ptr<Spielsteine> meinStein); //meinStein -> Beispielstein für den Verwendeten?
+	static std::shared_ptr<Spielsteine> senkrechtCheck(Spielfeld& spielfeld, std::shared_ptr<Spielsteine> meinStein);  //Eher über Übergabe des Teams?
+	static std::shared_ptr<Spielsteine> diagonalCheck(Spielfeld& spielfeld, std::shared_ptr<Spielsteine> meinStein);
 };
