070b784c9e54e64f01c3d69bdbfae45977df56f4
haasmi <64859108+haasmi@users.noreply.github.com>
2020-07-01 15:36:28 +0200

    Gewinnausgabe

--- Code/VierGewinnt/VierGewinntGameLibrary/GameController.cpp
+++ Code/VierGewinnt/VierGewinntGameLibrary/GameController.cpp
@@ -21,7 +21,13 @@ void GameController::playGame() //Erstmal: spielen bis Feld voll
 		win = playStep();
 	} while (!m_spielfeld.isVoll() && !win);
 	ConsolePrinter::printField(m_spielfeld);
-	ConsolePrinter::printMessage("Feld voll");
+	if (win) {
+		ConsolePrinter::printMessage(m_spielerDran->getName());
+		ConsolePrinter::printMessage("hat gewonnen");
+	}
+	else {
+		ConsolePrinter::printMessage("Feld voll");
+	}
 }
 
 bool GameController::playStep()
