5811909287f9d81303bdcca16530d5aeeada8619
LucRome <64947931+LucRome@users.noreply.github.com>
2020-07-07 23:02:07 +0200

    überflüssige Funktionen aus Regelwekr entfernt

--- Code/VierGewinnt/VierGewinntGameLibrary/Regelwerk.cpp
+++ Code/VierGewinnt/VierGewinntGameLibrary/Regelwerk.cpp
@@ -20,36 +20,8 @@ Regelwerk::~Regelwerk()
 }
 
 
-bool Regelwerk::gewonnen(const Spielfeld& m_spielfeld, const Team& team)
-{
-	bool gewinnen = false;
-	if (waagerechtCheck(m_spielfeld, team) != 0)
-	{
-		return !gewinnen;
-	}
-	if (senkrechtCheck(m_spielfeld, team) != 0)
-	{
-		return !gewinnen;
-	}
-	if (diagonalCheck(m_spielfeld, team) != 0)
-	{
-		return !gewinnen;
-	}
-	return gewinnen;
-}
-
 bool Regelwerk::gewonnen(const Coord coordinates, const Spielfeld& m_spielfeld, const Team& team)
 {
-	/*if (waagerechtCheck(coordinates, m_spielfeld, team)) {
-		return true;
-	}
-	if (senkrechtCheck(coordinates, m_spielfeld, team)) {
-		return true;
-	}
-	if (diagonalCheck(coordinates, m_spielfeld, team)) {
-		return true;
-	}
-	return false;*/
 	if (highestStreak(coordinates, m_spielfeld, team) == 3) {
 		return true;
 	}
@@ -82,96 +54,6 @@ int Regelwerk::highestStreak(const Coord coordinates, const Spielfeld& m_spielfe
 	return max;
 }
 
-bool Regelwerk::waagerechtCheck(const Spielfeld& m_spielfeld, const Team& team)
-{
-	int zähler = 0;	//keine Umlaute!!!
-	int max_zähler = 0;
-	std::shared_ptr<Spielsteine> jetztDa;
-	Coord size = m_spielfeld.getSize();
-	for (int zeile = 0; zeile < size.zeile; zeile++) {
-		for (int spalte = 0; spalte < size.spalte; spalte++) {
-			if (umgebungBool({ zeile, spalte }, d_waagerecht, m_spielfeld, team) == true)
-			{
-				return true;
-			}
-		}
-	}
-	return false;
-}
-
-bool Regelwerk::senkrechtCheck(const Spielfeld& m_spielfeld, const Team& team)
-{
-	int zähler = 0;
-	int max_zähler = 0;
-	Coord size = m_spielfeld.getSize();
-	std::shared_ptr<Spielsteine> jetztDa;
-	for (int zeile = 0; zeile < size.zeile; zeile++) {
-		for (int spalte = 0; spalte < size.spalte; spalte++) {
-			if (umgebungBool({ zeile, spalte }, d_senkrecht, m_spielfeld, team) == true)
-			{
-				return true;
-			}
-		}
-	}
-	return false;
-}
-
-bool Regelwerk::diagonalCheck(const Spielfeld& m_spielfeld, const Team& team)
-{
-	int zähler = 0;
-	int max_zähler = 0;
-	Coord size = m_spielfeld.getSize();
-	std::shared_ptr<Spielsteine> jetztDa;
-	for (int zeile = 0; zeile < size.zeile; zeile++) {
-		for (int spalte = 0; spalte < size.spalte; spalte++) {
-			if (umgebungBool({ zeile, spalte }, d_diagonal1, m_spielfeld, team) == true)
-			{
-				return true;
-			}
-		}
-	}
-
-	for (int zeile = 0; zeile < size.zeile; zeile++) {
-		for (int spalte = 0; spalte < size.spalte; spalte++) {
-			if (umgebungBool({ zeile, spalte }, d_diagonal2, m_spielfeld, team) == true)
-			{
-				return true;
-			}
-		}
-	}
-
-	return false;
-}
-
-bool Regelwerk::waagerechtCheck(const Coord coordinates, const Spielfeld& m_spielfeld, const Team& team)
-{
-	if (umgebungBool(coordinates, d_waagerecht, m_spielfeld, team)) {
-		return true;
-	}
-	else {
-		return false;
-	}
-}
-
-bool Regelwerk::senkrechtCheck(const Coord coordinates, const Spielfeld& m_spielfeld, const Team& team)
-{
-	if (umgebungBool(coordinates, d_senkrecht, m_spielfeld, team)) {
-		return true;
-	}
-	else {
-		return false;
-	}
-}
-
-bool Regelwerk::diagonalCheck(const Coord coordinates, const Spielfeld& m_spielfeld, const Team& team)
-{
-	if (umgebungBool(coordinates, d_diagonal1, m_spielfeld, team) || umgebungBool(coordinates, d_diagonal2, m_spielfeld, team)) {
-		return true;
-	}
-	else {
-		return false;
-	}
-}
 
 int Regelwerk::umgebungInt(const Coord coordinates, Coord deltas, const Spielfeld& m_spielfeld, const Team& team)
 {
@@ -181,41 +63,9 @@ int Regelwerk::umgebungInt(const Coord coordinates, Coord deltas, const Spielfel
 	deltas.zeile = -deltas.zeile;
 	cnt2 = countNeighbours(coordinates, deltas, m_spielfeld, team);
 
-	/*if (cnt1 < cnt2) {
-		return cnt2;
-	}
-	else {
-		return cnt1;
-	}*/
 	return cnt1 + cnt2;
 }
 
-
-bool Regelwerk:: umgebungBool(const Coord coordinates, Coord deltas, const Spielfeld& m_spielfeld, const Team& team)
-{
-	std::shared_ptr<Spielsteine> jetztDa;
-	int counter=0;
-	int max_counter=0;
-	counter = countNeighbours(coordinates, deltas, m_spielfeld, team);
-	if (counter == 3) {
-		m_spielfeld.getPosition(coordinates)->setZähler(counter);
-		return true;
-	}
-	deltas.spalte = -deltas.spalte;
-	deltas.zeile = -deltas.zeile;
-	counter = countNeighbours(coordinates, deltas, m_spielfeld, team);
-	if (counter == 3) {
-		m_spielfeld.getPosition(coordinates)->setZähler(counter);
-		return true;
-	}
-	else if (max_counter < counter) {
-		m_spielfeld.getPosition(coordinates)->setZähler(counter);
-		max_counter = counter;
-		jetztDa = m_spielfeld.getPosition(coordinates);
-		return false;
-	}
-	return false;
-}
 
 bool Regelwerk::ImSpielfeld(const Coord coordinates, const Spielfeld& m_spielfeld)
 {
@@ -233,17 +83,6 @@ int Regelwerk::countNeighbours(Coord coordinates, const Coord deltas, const Spie
 {
 	int counter = 0;
 	coordinates += deltas;
-	/*for (int i = 0; i < winNr; i++) {
-		if (ImSpielfeld(coordinates += deltas, m_spielfeld) && m_spielfeld.getPosition(coordinates) 
-			&& (team.getSymbol()) == (m_spielfeld.getPosition(coordinates))->getSymbol()) {
-
-			counter++;
-			coordinates += deltas;
-		}
-		else {
-			break;
-		}
-	}*/
 	if (ImSpielfeld(coordinates, m_spielfeld) && (m_spielfeld.getPosition(coordinates)))
 		//immer prüfen ob Zielfeld noch im Spielfeld und ob Ziehlfeld != nullptr
 	{
@@ -259,8 +98,6 @@ int Regelwerk::countNeighbours(Coord coordinates, const Coord deltas, const Spie
 					if (ImSpielfeld(coordinates, m_spielfeld) && (m_spielfeld.getPosition(coordinates)))
 					{
 						if ((team.getSymbol()) == (m_spielfeld.getPosition(coordinates))->getSymbol()) {
-							/*Umgebung1 += zähler1;
-							Umgebung2 += zähler2;*/
 							counter++;
 						}
 					}
